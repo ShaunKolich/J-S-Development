9428f487a370bcd191cff79904cc7e489a568adb
ShaunKolich <employmeinc@gmail.com>
2019-06-15 18:37:58 -0400

    Setting up Website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WebsiteDisplay from '../src/pages/websitedisplay';
+
+function App() {
+  return (
+    <div className="App">
+     
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//import { Container, Row, Col } from 'fluid-react';
+
+class Home extends Component {
+    render() {
+        return (
+            
+
+
+            
+        )
+    }
+}
